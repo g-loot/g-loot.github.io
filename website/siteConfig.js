08a4deb7cfd5704651b831ec97770b7467bfba33
Thomas Guibert <thomas.guibert@gloot.com>
2019-09-10 09:41:58 +0200

    change current_branch and add CNAME to siteconfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -110,6 +110,7 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+  cname: 'developers.gloot.com',
 };
 
 module.exports = siteConfig;
