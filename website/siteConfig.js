42979811ead39d8f19de234277828be1b1f8673f
Thomas Guibert <thomas.guibert@gloot.com>
2019-09-12 14:18:19 +0200

    Adding Segment Analytic Tag

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -100,7 +100,7 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ['https://buttons.github.io/buttons.js', '/js/segment.analytics.js'],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
